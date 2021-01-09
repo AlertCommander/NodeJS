fd60b2147a4f0b95bfc92b7ea0606716c6b5256a
Nikola Maslac <7049@htl.rennweg.at>
2021-01-09 20:20:41 +0100

    Escalation Levels implementiert.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const recipients = ['+436503552474'];
 const tokens = ['abc123'];
 const device = '/dev/ttyUSB0';
+const escalation_levels = ["Emergency", "Alert", "Critical", "Error", "Warning", "Notice", "Informational", "Debug"]
+
 
 const serialportgsm = require('serialport-gsm');
 const express = require('express');
@@ -14,6 +16,10 @@ app.get('/send', (req, res) => {
   try {
     msg = req.query.msg;
     token = req.query.token;
+    level = req.query.level;
+
+    msg = escalation_levels[level] + "! \n" + msg;
+
     if (!tokens.includes(token))
       return res.send('unauthorized');
     for (const r of recipients)
